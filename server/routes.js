0da154a4d3656cd5c5f833c79c7850495efb6026
Badhusa <86298884+Badhusa@users.noreply.github.com>
2023-03-24 19:30:12 +0530

    additinal changes

--- server/routes.js
+++ server/routes.js
@@ -24,30 +24,26 @@ const api_token = process.env.ASSEMBLYAI_API_TOKEN;
 const client = new assemblyai.Client(api_token);
 
 router.post('/upload', upload.single('audio'), async (req, res) => {
-    try {
-      // Transcribe the audio file using AssemblyAI
-      const { data } = await client.transcribe({
-        audio_url: req.file.path,
-        speaker_labels: true,
-        language_model: 'assemblyai_default',
-      });
-  
-      // Save the transcription to the database
-      const transcription = new Transcription({
-        fileName: req.file.originalname,
-        text: data.text,
-        confidence: data.confidence,
-        words: data.words,
-      });
-      await transcription.save();
-  
-      // Return the transcription as a response
-      res.json(data);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Error transcribing audio' });
-    }
-  });
+  try {
+    const { data } = await client.transcribe({
+      audio_url: req.file.path,
+      speaker_labels: true,
+      language_model: 'assemblyai_default',
+    });
+    const transcript = data.text;
+    const transcription = {
+      transcript,
+      createdAt: new Date()
+    };
+    const result = await db.collection('transcriptions').insertOne(transcription);
+    console.log(`Inserted ${result.insertedCount} documents`);
+    res.json(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Error transcribing audio' });
+  }
+});
+
   
 
 module.exports = router;
